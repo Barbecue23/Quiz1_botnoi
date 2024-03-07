3d0821b978794eccd99b09dfbfdf2c29f1c323b6
Barbecue23 <sunsanasupphong_j@silpakorn.com>
2024-03-07 22:27:42 +0700

    first commit

---
+++ quiz/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function NumberInput() {
+  const [number, setNumber] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleConfirm = () => {
+    const value = parseInt(inputValue);
+    if (!isNaN(value) && value >= 1) {
+      let asterisks = '<br>';
+      for (let i = 1; i <= value; i++) {
+        asterisks += '*'.repeat(i) + '<br>'; // เพิ่ม '*' และขึ้นบรรทัดใหม่ในแต่ละรอบ
+      }
+      for (let i = value-1; i >= 1; i--) {
+        asterisks += '*'.repeat(i) + '<br>'; // ลด '*' และขึ้นบรรทัดใหม่ในแต่ละรอบ
+      }
+      setNumber(`เหลือ ${asterisks}`);
+    } else {
+      setNumber('');
+    }
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <label className="inputContainer" htmlFor="numberInput">รับจำนวน:</label>
+        <input
+          className="inputnumber"
+          type="number"
+          id="numberInput"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button className="confirmButton" onClick={handleConfirm}>ตกลง</button>
+      </div>
+      <div className='result' dangerouslySetInnerHTML={{ __html: number }}></div>
+    </div>
+  );
+}
+
+export default NumberInput;
